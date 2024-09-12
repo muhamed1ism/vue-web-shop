0c3db8cb2b1e032680a88315edfaf07c2aec4bd2
Muhamed Spahić <muhamed1ism@gmail.com>
2024-09-12 03:26:18 +0200

    Ažuriran kontroler za narudžbe i dodana kontrola zaliha artikla pri naružbi

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,24 +1,56 @@
+import { OrderStatus } from '@prisma/client';
 import prisma from '../config/db.js';
 
 export const createOrder = async (req, res) => {
-  const { items, total } = req.body;
+  const { items } = req.body;
   const userId = req.user.id;
+
   try {
-    const newOrder = await prisma.order.create({
-      data: {
-        userId,
-        total,
-        status: 'PENDING',
-        items: {
-          create: items.map(item => ({
-            productId: item.productId,
-            quantity: item.quantity,
-            price: item.price,
-          })),
-        },
-      },
-    });
-    res.status(201).json(newOrder);
+    await prisma.$transaction(async (prisma) => {
+      let total = 0;
+      const orderItems = [];
+
+      for (const item of items) {
+        const product = await prisma.product.findUnique({
+          where: { id: item.productId },
+        });
+
+        if (!product || product.stock < item.quantity) {
+          return res.status(400).json({ message: `Nedovoljana zaliha za artikal: ${product.name}` });
+        }
+
+        total += product.price * item.quantity;
+
+        orderItems.push({
+          productId: product.id,
+          quantity: item.quantity,
+          price: product.price,
+        });
+
+        await prisma.product.update({
+          where: { id: product.id },
+          data: {
+            stock: product.stock - item.quantity,
+          },
+        });
+
+        const order = await prisma.order.create({
+          data: {
+            userId: userId,
+            total: total,
+            status: 'PENDING',
+            items: {
+              create: orderItems,
+            },
+          },
+          include: {
+            items: true,
+          },
+        });
+
+        res.status(201).json(order);
+      }
+    })
   } catch (error) {
     res.status(500).json({ error: 'Kreiranje narudžbe nije uspjelo' });
   }
@@ -26,11 +58,21 @@ export const createOrder = async (req, res) => {
 
 export const getUserOrders = async (req, res) => {
   const userId = req.user.id;
+
   try {
     const orders = await prisma.order.findMany({
       where: { userId },
-      include: { items: true },
+      include: {
+        items: {
+          select: {
+            productId: true,
+            quantity: true,
+            price: true,
+          }
+        },
+      },
     });
+
     res.status(200).json(orders);
   } catch (error) {
     res.status(500).json({ error: 'Dohvaćanje narudžbe nije uspjelo' });
@@ -40,7 +82,17 @@ export const getUserOrders = async (req, res) => {
 export const getAllOrders = async (req, res) => {
   try {
     const orders = await prisma.order.findMany({
-      include: { items: true, user: true },
+      include: {
+        items: {
+          select: {
+            id: true,
+            productId: true,
+            quantity: true,
+            price: true
+          },
+        },
+        user: true
+      },
     });
     res.status(200).json(orders);
   } catch (error) {
@@ -51,11 +103,17 @@ export const getAllOrders = async (req, res) => {
 export const updateOrderStatus = async (req, res) => {
   const { id } = req.params;
   const { status } = req.body;
+
+  if (!Object.values(OrderStatus).includes(status)) {
+    return res.status(400).json({ error: 'Dani status je pogrešan' })
+  }
+
   try {
     const updatedOrder = await prisma.order.update({
       where: { id: parseInt(id) },
       data: { status },
     });
+
     res.status(200).json(updatedOrder);
   } catch (error) {
     res.status(500).json({ error: 'Ažuriranje narudžbe nije uspjelo' });
